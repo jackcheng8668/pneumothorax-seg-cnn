51b9eeebdd739f5f6080e217050b58a03cb486df
jchen42703 <jchen42703@gmail.com>
2019-07-18 14:27:50 -0400

    Allow user to specify which metric to monitor for ModelCheckpoint and the corresponding mode
    Also changed all of the strings from ' to "

--- efficientnet_seg/training/callbacks.py
+++ efficientnet_seg/training/callbacks.py
@@ -11,8 +11,8 @@ class SWA(callbacks.Callback):
         self.swa_epoch = swa_epoch
 
     def on_train_begin(self, logs=None):
-        self.nb_epoch = self.params['epochs']
-        print('Stochastic weight averaging selected for last {} epochs.'
+        self.nb_epoch = self.params["epochs"]
+        print("Stochastic weight averaging selected for last {} epochs."
               .format(self.nb_epoch - self.swa_epoch))
 
     def on_epoch_end(self, epoch, logs=None):
@@ -30,9 +30,9 @@ class SWA(callbacks.Callback):
 
     def on_train_end(self, logs=None):
         self.model.set_weights(self.swa_weights)
-        print('Final model parameters set to stochastic weight average.')
+        print("Final model parameters set to stochastic weight average.")
         self.model.save_weights(self.filepath)
-        print('Final stochastic averaged weights saved to file.')
+        print("Final stochastic averaged weights saved to file.")
 
 class CyclicLR(callbacks.Callback):
     """This callback implements a cyclical learning rate policy (CLR).
@@ -53,7 +53,7 @@ class CyclicLR(callbacks.Callback):
     # Example
         ```python
             clr = CyclicLR(base_lr=0.001, max_lr=0.006,
-                                step_size=2000., mode='triangular')
+                                step_size=2000., mode="triangular")
             model.fit(X_train, Y_train, callbacks=[clr])
         ```
 
@@ -62,7 +62,7 @@ class CyclicLR(callbacks.Callback):
             clr_fn = lambda x: 0.5*(1+np.sin(x*np.pi/2.))
             clr = CyclicLR(base_lr=0.001, max_lr=0.006,
                                 step_size=2000., scale_fn=clr_fn,
-                                scale_mode='cycle')
+                                scale_mode="cycle")
             model.fit(X_train, Y_train, callbacks=[clr])
         ```
     # Arguments
@@ -78,23 +78,23 @@ class CyclicLR(callbacks.Callback):
             half cycle. Authors suggest setting step_size
             2-8 x training iterations in epoch.
         mode: one of {triangular, triangular2, exp_range}.
-            Default 'triangular'.
+            Default "triangular".
             Values correspond to policies detailed above.
             If scale_fn is not None, this argument is ignored.
-        gamma: constant in 'exp_range' scaling function:
+        gamma: constant in "exp_range" scaling function:
             gamma**(cycle iterations)
         scale_fn: Custom scaling policy defined by a single
             argument lambda function, where
             0 <= scale_fn(x) <= 1 for all x >= 0.
             mode paramater is ignored
-        scale_mode: {'cycle', 'iterations'}.
+        scale_mode: {"cycle", "iterations"}.
             Defines whether scale_fn is evaluated on
             cycle number or cycle iterations (training
-            iterations since start of cycle). Default is 'cycle'.
+            iterations since start of cycle). Default is "cycle".
     """
 
-    def __init__(self, base_lr=0.001, max_lr=0.006, step_size=2000., mode='triangular',
-                 gamma=1., scale_fn=None, scale_mode='cycle'):
+    def __init__(self, base_lr=0.001, max_lr=0.006, step_size=2000., mode="triangular",
+                 gamma=1., scale_fn=None, scale_mode="cycle"):
         super(CyclicLR, self).__init__()
 
         self.base_lr = base_lr
@@ -103,15 +103,15 @@ class CyclicLR(callbacks.Callback):
         self.mode = mode
         self.gamma = gamma
         if scale_fn == None:
-            if self.mode == 'triangular':
+            if self.mode == "triangular":
                 self.scale_fn = lambda x: 1.
-                self.scale_mode = 'cycle'
-            elif self.mode == 'triangular2':
+                self.scale_mode = "cycle"
+            elif self.mode == "triangular2":
                 self.scale_fn = lambda x: 1/(2.**(x-1))
-                self.scale_mode = 'cycle'
-            elif self.mode == 'exp_range':
+                self.scale_mode = "cycle"
+            elif self.mode == "exp_range":
                 self.scale_fn = lambda x: gamma**(x)
-                self.scale_mode = 'iterations'
+                self.scale_mode = "iterations"
         else:
             self.scale_fn = scale_fn
             self.scale_mode = scale_mode
@@ -137,7 +137,7 @@ class CyclicLR(callbacks.Callback):
     def clr(self):
         cycle = np.floor(1+self.clr_iterations/(2*self.step_size))
         x = np.abs(self.clr_iterations/self.step_size - 2*cycle + 1)
-        if self.scale_mode == 'cycle':
+        if self.scale_mode == "cycle":
             return self.base_lr + (self.max_lr-self.base_lr)*np.maximum(0, (1-x))*self.scale_fn(cycle)
         else:
             return self.base_lr + (self.max_lr-self.base_lr)*np.maximum(0, (1-x))*self.scale_fn(self.clr_iterations)
@@ -156,8 +156,8 @@ class CyclicLR(callbacks.Callback):
         self.trn_iterations += 1
         self.clr_iterations += 1
 
-        self.history.setdefault('lr', []).append(K.get_value(self.model.optimizer.lr))
-        self.history.setdefault('iterations', []).append(self.trn_iterations)
+        self.history.setdefault("lr", []).append(K.get_value(self.model.optimizer.lr))
+        self.history.setdefault("iterations", []).append(self.trn_iterations)
 
         for k, v in logs.items():
             self.history.setdefault(k, []).append(v)
@@ -170,11 +170,11 @@ class SnapshotCallbackBuilder:
         self.M = nb_snapshots
         self.alpha_zero = init_lr
 
-    def get_callbacks(self, swa, model_prefix='Model'):
+    def get_callbacks(self, swa, monitor="val_my_iou_metric", mode="max", model_prefix="Model"):
 
         callback_list = [
-            callbacks.ModelCheckpoint("./keras.model",monitor='val_my_iou_metric',
-                                   mode='max', save_best_only=True, verbose=1),
+            callbacks.ModelCheckpoint("./keras.model", monitor=monitor,
+                                   mode=mode, save_best_only=True, verbose=1),
             swa,
             callbacks.LearningRateScheduler(schedule=self._cosine_anneal_schedule)
         ]
