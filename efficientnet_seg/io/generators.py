a972e56f35c506cb006e857a9409aa7f82bf7c13
jchen42703 <jchen42703@gmail.com>
2019-07-18 14:03:24 -0400

    Fixed import

--- efficientnet_seg/io/generators.py
+++ efficientnet_seg/io/generators.py
@@ -3,7 +3,7 @@ import glob
 import pandas as pd
 import tensorflow as tf
 keras = tf.keras
-from efficientnet_seg.io.gen_utils import BaseGenerator
+from efficientnet_seg.io.base_generators import BaseGenerator
 from PIL import Image
 
 class SegmentationGenerator(BaseGenerator):
