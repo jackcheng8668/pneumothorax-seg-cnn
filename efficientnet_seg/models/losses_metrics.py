8b58dd54db2e57f68d0a64100db272d2439ebbf8
jchen42703 <jchen42703@gmail.com>
2019-07-21 17:33:04 -0400

    Bug fix for the F1 metric

--- efficientnet_seg/models/losses_metrics.py
+++ efficientnet_seg/models/losses_metrics.py
@@ -1,11 +1,7 @@
 import tensorflow.keras.backend as K
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras.losses import binary_crossentropy
-
-def f1(y_true, y_pred):
-    precision = precision(y_true, y_pred)
-    recall = recall(y_true, y_pred)
-    return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
 def recall(y_true, y_pred):
     """Recall metric.
@@ -32,6 +28,11 @@ def precision(y_true, y_pred):
     predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
     precision = true_positives / (predicted_positives + K.epsilon())
     return precision
+
+def f1(y_true, y_pred):
+    ppv = precision(y_true, y_pred)
+    tpr = recall(y_true, y_pred)
+    return 2*((ppv*tpr)/(ppv+tpr+K.epsilon()))
 
 def dice_coef(y_true, y_pred):
     y_true_f = K.flatten(y_true)
