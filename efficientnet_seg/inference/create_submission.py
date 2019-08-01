5c8e3b5c4569124ed7c9d943bdcdedd17b8c7fbe
jchen42703 <jchen42703@gmail.com>
2019-08-01 18:44:23 -0400

    Moved the mask functions out of create_submission and deleted the redundant preprocessing_input function
    Did so for cleaner code

--- efficientnet_seg/inference/create_submission.py
+++ efficientnet_seg/inference/create_submission.py
@@ -8,6 +8,7 @@ import pandas as pd
 from pathlib import Path
 from PIL import Image
 from tqdm import tqdm
+from efficientnet_seg.inference.mask_functions import *
 
 """
 To-do:
@@ -189,33 +190,6 @@ def data_augmentation(image):
     if len(image.shape) == 2: image = np.expand_dims(image, axis=2)
     return image
 
-def preprocess_input(x, model):
-    x = x.astype("float32")
-    if model in ("inception","xception","mobilenet"):
-        x /= 255.
-        x -= 0.5
-        x *= 2.
-    if model in ("densenet"):
-        x /= 255.
-        if x.shape[-1] == 3:
-            x[..., 0] -= 0.485
-            x[..., 1] -= 0.456
-            x[..., 2] -= 0.406
-            x[..., 0] /= 0.229
-            x[..., 1] /= 0.224
-            x[..., 2] /= 0.225
-        elif x.shape[-1] == 1:
-            x[..., 0] -= 0.449
-            x[..., 0] /= 0.226
-    elif model in ("resnet","vgg"):
-        if x.shape[-1] == 3:
-            x[..., 0] -= 103.939
-            x[..., 1] -= 116.779
-            x[..., 2] -= 123.680
-        elif x.shape[-1] == 1:
-            x[..., 0] -= 115.799
-    return x
-
 def load_input(fpath, img_size=256):
     """
     Loads and resizes a .png file.
@@ -230,46 +204,3 @@ def load_input(fpath, img_size=256):
         arr = cv2.resize(arr, resize_shape)
     # repeating for RGB inputs
     return np.repeat(arr[...,None], 3, 2)
-
-import numpy as np
-
-def mask2rle(img, width, height):
-    rle = []
-    lastColor = 0;
-    currentPixel = 0;
-    runStart = -1;
-    runLength = 0;
-
-    for x in range(width):
-        for y in range(height):
-            currentColor = img[x][y]
-            if currentColor != lastColor:
-                if currentColor == 255:
-                    runStart = currentPixel;
-                    runLength = 1;
-                else:
-                    rle.append(str(runStart));
-                    rle.append(str(runLength));
-                    runStart = -1;
-                    runLength = 0;
-                    currentPixel = 0;
-            elif runStart > -1:
-                runLength += 1
-            lastColor = currentColor;
-            currentPixel+=1;
-
-    return " ".join(rle)
-
-def rle2mask(rle, width, height):
-    mask= np.zeros(width* height)
-    array = np.asarray([int(x) for x in rle.split()])
-    starts = array[0::2]
-    lengths = array[1::2]
-
-    current_position = 0
-    for index, start in enumerate(starts):
-        current_position += start
-        mask[current_position:current_position+lengths[index]] = 255
-        current_position += lengths[index]
-
-    return mask.reshape(width, height)
