c34b2a6d0b831e5d20442175c2568303b3d4bc39
jchen42703 <jchen42703@gmail.com>
2019-08-02 12:31:47 -0400

    Refactored the Grayscale generators to preprocess AFTER augmentation instead of BEFORE
    Just makes more sense because both the data_augmentation functions from data_aug and the transforms from albumentations are for unscaled (0, 255) np.ndarrays.

--- efficientnet_seg/io/generators_grayscale.py
+++ efficientnet_seg/io/generators_grayscale.py
@@ -1,6 +1,8 @@
 import numpy as np
 import glob
 import tensorflow as tf
+import albumentations
+
 from efficientnet_seg.io.generators import SegmentationGenerator, ClassificationGenerator
 from PIL import Image
 
@@ -14,6 +16,8 @@ class GrayscaleSegmentationGenerator(SegmentationGenerator):
         model_name (str): Either 'densenet', 'inception', or 'xception' to specify the preprocessing
         fpaths (list): of filepaths directly to the training images
         augmentations (albumentations transform): either Composed or an individual augmentation
+            * Note: This can also just be a function; must take in the `image` and `mask` arguments and
+            return a dictionary with the keys: `image`, `mask`. An example is `io.data_aug.data_augmentation_all`.
         shuffle (bool):
     """
     def __init__(self, images_dir, masks_dir, batch_size, model_name=None, fpaths=None, augmentations=None, shuffle=True):
@@ -21,6 +25,35 @@ class GrayscaleSegmentationGenerator(SegmentationGenerator):
         super().__init__(images_dir=images_dir, masks_dir=masks_dir, batch_size=batch_size, fpaths=fpaths, \
                          augmentations=augmentations, shuffle=shuffle)
         self.on_epoch_end()
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size: min((index+1)*self.batch_size, len(self.fpaths))]
+
+        # Find list of IDs
+        fpaths_temp = [self.fpaths[k] for k in indexes]
+
+        # Generate data
+        X, Y = self.data_gen(fpaths_temp)
+
+        # only preprocesses the input when there is no data augmentation
+        if self.augment is None:
+            if self.model_name is not None:
+                X = preprocess_input(X, self.model_name)
+            return X, np.array(Y)/255
+        else:
+            # Augmentation
+            im, masks = [], []
+            for x,y in zip(X,Y):
+                augmented = self.augment(image=x, mask=y)
+                # The output of a self.augment should be a dictionary {"image":..., "mask":...}
+                im.append(augmented['image']), masks.append(augmented['mask'])
+            X, Y = np.asarray(im), np.asarray(masks)/255
+            # preprocessing
+            if self.model_name is not None:
+                X = preprocess_input(X, self.model_name)
+            return X, Y
 
     def data_gen(self, fpaths_temp):
         """
@@ -40,8 +73,6 @@ class GrayscaleSegmentationGenerator(SegmentationGenerator):
             x = np.array(Image.open(fpath))[..., np.newaxis]
             mask_path = fpath.replace(self.images_dir, self.masks_dir)
             y = np.array(Image.open(mask_path))[..., np.newaxis]
-            if self.model_name is not None:
-                x = preprocess_input(x, self.model_name)
             y[y>0] = 255 # does this for data augmentation purposes
             x_batch.append(x), y_batch.append(y)
         X, Y = np.stack(x_batch), np.stack(y_batch)
@@ -56,6 +87,8 @@ class GrayscaleClassificationGenerator(ClassificationGenerator):
         batch_size (int):
         fpaths (list): of filepaths directly to the training images
         augmentations (albumentations transform): either Composed or an individual augmentation
+            * Note: This can also just be a function; must take in the `image` and `mask` arguments and
+            return a dictionary with the keys: `image`, `mask`. An example is `io.data_aug.data_augmentation_all`.
         shuffle (bool):
     """
     def __init__(self, images_dir, masks_dir, batch_size, model_name=None, fpaths=None, augmentations=None, shuffle=True):
@@ -63,6 +96,30 @@ class GrayscaleClassificationGenerator(ClassificationGenerator):
         super().__init__(images_dir=images_dir, masks_dir=masks_dir, batch_size=batch_size, fpaths=fpaths, \
                          augmentations=augmentations, shuffle=shuffle)
         self.on_epoch_end()
+
+    def __getitem__(self, idx):
+        """
+        Defines the fetching and on-the-fly preprocessing of data.
+        Args:
+            idx: the id assigned to each worker
+        Returns:
+            (X,Y): a batch of transformed data/labels
+        """
+        indexes = self.indexes[idx*self.batch_size:(idx+1)*self.batch_size]
+        # Fetches batched IDs for a thread
+        fpaths_temp = [self.fpaths[k] for k in indexes]
+        X, Y = self.data_gen(fpaths_temp)
+        # data augmentation
+        if self.augment is None:
+            if self.model_name is not None:
+                X = preprocess_input(X, self.model_name)
+            return (X, Y)
+        else:
+            # The output of a self.augment should be a dictionary {"image":..., "mask":...}
+            X = np.stack([self.augment(image=x)['image'] for x in X])
+            if self.model_name is not None:
+                X = preprocess_input(X, self.model_name)
+            return (X, Y)
 
     def data_gen(self, fpaths_temp):
         """
@@ -77,8 +134,6 @@ class GrayscaleClassificationGenerator(ClassificationGenerator):
         for fpath in fpaths_temp:
             # loads data as a numpy arr and then adds the channel + batch size dimensions
             x = np.array(Image.open(fpath))[..., np.newaxis]
-            if self.model_name is not None:
-                x = preprocess_input(x, self.model_name)
             # creating the classification label from the segmentation mask
             mask_path = fpath.replace(self.images_dir, self.masks_dir)
             y = np.array(Image.open(mask_path))[..., np.newaxis]
