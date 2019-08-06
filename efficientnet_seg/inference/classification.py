6c7d8bb013991e16251780e3f36c050dd8e5c29e
jchen42703 <jchen42703@gmail.com>
2019-08-06 16:50:36 -0400

    Added documentation and ensembling potential for Stage1
    Also fixed some of the `` so that they become ""

--- efficientnet_seg/inference/classification.py
+++ efficientnet_seg/inference/classification.py
@@ -5,20 +5,53 @@ import pandas as pd
 from pathlib import Path
 from efficientnet_seg.io.data_aug import data_augmentation
 from efficientnet_seg.inference.utils import load_input
+from efficientnet_seg.io.generators_grayscale import preprocess_input
 
 def Stage1(classification_model, test_fpaths, channels=3, img_size=256, batch_size=32, tta=True,
-           threshold=0.5):
+           threshold=0.5, model_name=None):
+    """
+    For the first (classification) stage of the classification/segmentation cascade. It assumes that the
+    classification_model was trained on the regular dataset.
+
+    Args:
+        classification_model (a single tf.keras.model.Model or keras.model.Model or a list of them): assumes
+            that they all need the same input. When `classification_model` is a list/tuple, the models are
+            ensembled (predictions are averaged.)
+        test_fpaths (list or tuple): of file paths to the test images
+        channels (int): The number of input channels. Defaults to 3.
+        img_size (int): The size of each square input image. Defaults to 256.
+        batch_size (int): model prediction batch size
+        tta (boolean): whether or not to apply test-time augmentation.
+        threshold (float): threshold for the predicted arrays where
+            anything >= threshold = 255 and anything < threshold = 0. Defaults to 0.5.
+        model_name (str): Either 'densenet', 'inception', or 'xception' to specify the preprocessing method
+    Returns:
+        sub_df (pd.DataFrame): the classification submission data frame (Encoded pixels are 1/-1 for pneumothorax/no pneumothorax).
+    """
     # Stage 1: Classification predictions
     print("Commencing Stage 1: Prediction of Pneumothorax or No Pneumothorax Patients")
     # Load test set
     x_test = np.asarray([load_input(fpath, img_size, channels=channels) for fpath in test_fpaths])
-
+    if model_name is not None:
+        x_test = preprocess_input(x_test, model_name)
     ## Hacky fix for binary cases where the output is (N, 1)
     ### Prevents lists being saved as nested lists
     if tta:
-        preds_classify = TTA_Classification_All(classification_model, x_test, n_iters=4, batch_size=batch_size).flatten()
+        # ensembling with TTA
+        if isinstance(classification, (list, tuple)):
+            # stacking across the batch_size dimension
+            preds_classify = np.mean(np.vstack([TTA_Classification_All(model_, x_test, batch_size=batch_size)
+                                          for model_ in classification_model]), axis=0).flatten()
+        else:
+            preds_classify = TTA_Classification_All(classification_model, x_test, n_iters=4, batch_size=batch_size).flatten()
     else:
-        preds_classify = classification_model.predict(x_test, batch_size=batch_size).flatten()
+        # ensembling without TTA
+        if isinstance(classification_model, (list, tuple)):
+            # stacking across the batch_size dimension
+            preds_seg = np.mean(np.vstack([model_.predict(x_test, batch_size=batch_size)
+                                          for model_ in classification_model]), axis=0).flatten()
+        else:
+            preds_classify = classification_model.predict(x_test, batch_size=batch_size).flatten()
 
     test_ids = [Path(fpath).stem for fpath in test_fpaths] # for the df
     # thresholding
@@ -27,8 +60,8 @@ def Stage1(classification_model, test_fpaths, channels=3, img_size=256, batch_si
     # converting arr to a list of integers
     preds_classify = [int(pred) for pred in preds_classify.tolist()]
     # creating first df
-    sub_df = pd.DataFrame({'ImageId': test_ids, 'EncodedPixels': preds_classify})
-    sub_df.to_csv('submission_classification.csv', index=False)
+    sub_df = pd.DataFrame({"ImageId": test_ids, "EncodedPixels": preds_classify})
+    sub_df.to_csv("submission_classification.csv", index=False)
     print("Stage 1 Completed.")
     return sub_df
 
