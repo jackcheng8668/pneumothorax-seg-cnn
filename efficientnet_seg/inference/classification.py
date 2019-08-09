f4a1d9824221ab355c29a77fe4e9e2482d4971b8
jchen42703 <jchen42703@gmail.com>
2019-08-09 11:11:50 -0400

    Can now save the classification probabilities as a .csv file and also made the thresholding + df creation a separate function and fixed TTA for classification
    The probabilities can be saved using the `save_p` option. Also made a new function `create_thresholded_classification_csv()` to save the thresholded classification predictions as a .csv file for increased modularity. It's also for parallelism purposes because it looks cleaner with it and the `create_classification_p_df()` function.
    The bugs with TTA_Classification came from misnamed arguments (n_iter) and how data_augmentation returns a tuple of arrs instead of just a single arr.

--- efficientnet_seg/inference/classification.py
+++ efficientnet_seg/inference/classification.py
@@ -3,12 +3,13 @@ import numpy as np
 import pandas as pd
 
 from pathlib import Path
+from tqdm import tqdm
 from efficientnet_seg.io.data_aug import data_augmentation
 from efficientnet_seg.inference.utils import load_input
 from efficientnet_seg.io.generators_grayscale import preprocess_input
 
 def Stage1(classification_model, test_fpaths, channels=3, img_size=256, batch_size=32, tta=True,
-           threshold=0.5, model_name=None):
+           threshold=0.5, model_name=None, save_p=True):
     """
     For the first (classification) stage of the classification/segmentation cascade. It assumes that the
     classification_model was trained on the regular dataset.
@@ -25,6 +26,8 @@ def Stage1(classification_model, test_fpaths, channels=3, img_size=256, batch_si
         threshold (float): threshold for the predicted arrays where
             anything >= threshold = 255 and anything < threshold = 0. Defaults to 0.5.
         model_name (str): Either 'densenet', 'inception', or 'xception' to specify the preprocessing method
+        save_p (bool): whether or not to save the classification probabilties. If True, probabilities are saved
+            as a .csv file at cwd/classification_probabilties.csv
     Returns:
         sub_df (pd.DataFrame): the classification submission data frame (Encoded pixels are 1/-1 for pneumothorax/no pneumothorax).
     """
@@ -38,39 +41,58 @@ def Stage1(classification_model, test_fpaths, channels=3, img_size=256, batch_si
     ### Prevents lists being saved as nested lists
     if tta:
         # ensembling with TTA
-        if isinstance(classification, (list, tuple)):
+        if isinstance(classification_model, (list, tuple)):
             # stacking across the batch_size dimension
-            preds_classify = np.mean(np.vstack([TTA_Classification_All(model_, x_test, batch_size=batch_size)
+            preds_classify = np.mean(np.stack([TTA_Classification_All(model_, x_test, batch_size=batch_size)
                                           for model_ in classification_model]), axis=0).flatten()
         else:
-            preds_classify = TTA_Classification_All(classification_model, x_test, n_iters=4, batch_size=batch_size).flatten()
+            preds_classify = TTA_Classification_All(classification_model, x_test, n_iter=4, batch_size=batch_size).flatten()
     else:
         # ensembling without TTA
         if isinstance(classification_model, (list, tuple)):
             # stacking across the batch_size dimension
-            preds_seg = np.mean(np.vstack([model_.predict(x_test, batch_size=batch_size)
+            preds_seg = np.mean(np.stack([model_.predict(x_test, batch_size=batch_size)
                                           for model_ in classification_model]), axis=0).flatten()
         else:
             preds_classify = classification_model.predict(x_test, batch_size=batch_size).flatten()
 
     test_ids = [Path(fpath).stem for fpath in test_fpaths] # for the df
-    # thresholding
-    preds_classify[preds_classify >= threshold] = 1
-    preds_classify[preds_classify < threshold] = -1
-    # converting arr to a list of integers
-    preds_classify = [int(pred) for pred in preds_classify.tolist()]
-    # creating first df
-    sub_df = pd.DataFrame({"ImageId": test_ids, "EncodedPixels": preds_classify})
-    sub_df.to_csv("submission_classification.csv", index=False)
+    if save_p:
+        from efficientnet_seg.inference.ensemble_df import create_classification_p_df
+        _ = create_classification_p_df(preds_classify, test_ids)
+    sub_df = create_thresholded_classification_csv(preds_classify, test_ids, threshold=threshold)
     print("Stage 1 Completed.")
     return sub_df
 
-def TTA_Classification_All(model, test_arrays, n_iters=4, batch_size=32, seed=88):
+def create_thresholded_classification_csv(pred_arr, test_ids, threshold=0.5):
+    """
+    Creates the thresholded version of the classification predictions data frame (csv).
+    Args:
+        pred_arr (np.ndarray): 1D numpy array of predicted probabilities for each image
+            corresponding to each test_id.
+        test_ids (list): dicom ids corresponding to each predicted probability
+        threshold (float): threshold
+    Returns:
+        None
+    """
+    # thresholding
+    pred_arr[pred_arr >= threshold] = 1
+    pred_arr[pred_arr < threshold] = -1
+    # converting arr to a list of integers
+    preds_classify = [int(pred) for pred in pred_arr.tolist()]
+    # creating first df
+    sub_df = pd.DataFrame({"ImageId": test_ids, "EncodedPixels": preds_classify})
+    sub_df.to_csv("submission_classification.csv", index=False)
+    print("Classification csv saved at {0}".format(os.path.join(os.getcwd(), "submission_classification")))
+    return sub_df
+
+def TTA_Classification_All(model, test_arrays, n_iter=4, batch_size=32, seed=88):
     """
     Thin wrapper around for TTA Classification for TTA on all images instead of per image.
     """
-    return np.asarray([TTA(model, test_arr, n_iters=n_iters, batch_size=batch_size, seed=seed)
-                       for test_arr in test_arrays])
+    print("TTA for classification...")
+    return np.asarray([TTA_Classification(model, test_arr, n_iter=n_iter, batch_size=batch_size, seed=seed)
+                       for test_arr in tqdm(test_arrays)])
 
 def TTA_Classification(model, test_array, n_iter=4, batch_size=32, seed=88):
     """
@@ -87,7 +109,7 @@ def TTA_Classification(model, test_array, n_iter=4, batch_size=32, seed=88):
     # for reproducibility
     np.random.seed(seed)
     # creating arrays to store the augmented images
-    original_img_array = np.empty((niter+1, test_array.shape[0], test_array.shape[1], test_array.shape[2]))
+    original_img_array = np.empty((n_iter+1, test_array.shape[0], test_array.shape[1], test_array.shape[2]))
     inverted_img_array = original_img_array.copy()
     hflipped_img_array = original_img_array.copy()
     # 1st round of augmentations: flipping and inversion
@@ -99,10 +121,10 @@ def TTA_Classification(model, test_array, n_iter=4, batch_size=32, seed=88):
     hflipped_img_array[0] = hflipped_img
 
     # data augmentation: vertical flipping, gaussian smoothing, random rotations, and random zooms.
-    for each_iter in range(niter):
-        original_img_array[each_iter+1] = data_augmentation(original_img)
-        inverted_img_array[each_iter+1] = data_augmentation(inverted_img)
-        hflipped_img_array[each_iter+1] = data_augmentation(hflipped_img)
+    for each_iter in range(n_iter):
+        original_img_array[each_iter+1] = data_augmentation(original_img)[0] # no mask
+        inverted_img_array[each_iter+1] = data_augmentation(inverted_img)[0] # no mask
+        hflipped_img_array[each_iter+1] = data_augmentation(hflipped_img)[0] # no mask
     tmp_array = np.vstack((original_img_array, inverted_img_array, hflipped_img_array))
     # preprocessing // for now, no preprocessing
     # tmp_array = preprocess_input(tmp_array, model_name)
