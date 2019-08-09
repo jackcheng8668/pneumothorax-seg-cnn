f41709717044bf33ac1f4819bb2010a965b2dcec
jchen42703 <jchen42703@gmail.com>
2019-08-09 13:29:36 -0400

    Explicit type checking for model_name in preprocess_input
    To explicity skip further preprocessing if model_name is None

--- efficientnet_seg/io/utils.py
+++ efficientnet_seg/io/utils.py
@@ -17,30 +17,31 @@ def preprocess_input(x, model_name):
     Args:
         x (np.ndarray): (x, y, z, n_channels)
         model_name (str): Either `inception`, `xception`, `mobilenet`, `resnet`, `vgg`, or `densenet`.
-            If model_name == "efficientnet", then it will do no additional preprocessing.
+            Anything other than those strings will result in the array just being converted to a float.
     """
     x = x.astype("float32")
-    if model_name in ("inception","xception","mobilenet"):
-        x /= 255.
-        x -= 0.5
-        x *= 2.
-    if model_name in ("densenet"):
-        x /= 255.
-        if x.shape[-1] == 3:
-            x[..., 0] -= 0.485
-            x[..., 1] -= 0.456
-            x[..., 2] -= 0.406
-            x[..., 0] /= 0.229
-            x[..., 1] /= 0.224
-            x[..., 2] /= 0.225
-        elif x.shape[-1] == 1:
-            x[..., 0] -= 0.449
-            x[..., 0] /= 0.226
-    elif model_name in ("resnet","vgg"):
-        if x.shape[-1] == 3:
-            x[..., 0] -= 103.939
-            x[..., 1] -= 116.779
-            x[..., 2] -= 123.680
-        elif x.shape[-1] == 1:
-            x[..., 0] -= 115.799
+    if isinstance(model_name, str):
+        if model_name in ("inception","xception","mobilenet"):
+            x /= 255.
+            x -= 0.5
+            x *= 2.
+        if model_name in ("densenet"):
+            x /= 255.
+            if x.shape[-1] == 3:
+                x[..., 0] -= 0.485
+                x[..., 1] -= 0.456
+                x[..., 2] -= 0.406
+                x[..., 0] /= 0.229
+                x[..., 1] /= 0.224
+                x[..., 2] /= 0.225
+            elif x.shape[-1] == 1:
+                x[..., 0] -= 0.449
+                x[..., 0] /= 0.226
+        elif model_name in ("resnet","vgg"):
+            if x.shape[-1] == 3:
+                x[..., 0] -= 103.939
+                x[..., 1] -= 116.779
+                x[..., 2] -= 123.680
+            elif x.shape[-1] == 1:
+                x[..., 0] -= 115.799
     return x
