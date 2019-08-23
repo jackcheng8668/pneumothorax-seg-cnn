370b34746719b9c3697f249dd4baf8576137f807
jchen42703 <jchen42703@gmail.com>
2019-08-23 00:23:49 -0400

    Refactored functions so that there is less repeat code with preprocess_fn in the arguments
    Reduced the number of **kwargs occurrences in the functions arguments (only Stage1 has it) by making it so that Stage1 has it made into a partial'd function. Fixed documentation to accomodate and added support in TTA_Classification to handle cases where preprocess_fn is None.
    Also, fixed a bug where batch_test_fpaths was called instead of test_fpaths_batched.

--- efficientnet_seg/inference/classification.py
+++ efficientnet_seg/inference/classification.py
@@ -51,7 +51,7 @@ def Stage1(classification_model, test_fpaths, channels=3, img_size=256, batch_si
     # Load test set
     ## batching test_fpaths; # preserves order
     test_fpaths_batched = batch_test_fpaths(test_fpaths, batch_size=fpaths_batch_size)
-    print("{0} file batches with sizes {1}".format(len(test_fpaths_batched), [len(batch) for batch in batch_test_fpaths]))
+    print("{0} file batches with sizes {1}".format(len(test_fpaths_batched), [len(batch) for batch in test_fpaths_batched]))
     preds_classify = np.array([])
     for fpaths_batch in test_fpaths_batched:
         x_test = np.asarray([load_input(fpath, img_size, channels=channels) for fpath in fpaths_batch])
@@ -64,13 +64,16 @@ def Stage1(classification_model, test_fpaths, channels=3, img_size=256, batch_si
             ## Not entirely true ^, it does cast the inputs from int -> float32
             ## if need be.
             preprocess_fn = None
+        else:
+            preprocess_fn = partial(preprocess_input, model_name=model_name) if preprocess_fn is None \
+                            else partial(preprocess_fn, model_name=model_name, **kwargs)
         # predictions (with/without TTA)
         preds_classify_batch = run_classification_prediction(x_test, classification_model,
                                                              batch_size=batch_size, tta=tta,
                                                              n_tta_iter_per_image=n_tta_iter_per_image,
-                                                             preprocess_fn=preprocess_fn, **kwargs)
+                                                             preprocess_fn=preprocess_fn)
         # appending -> flattening
-        preds_classify = np.append([preds_classify, preds_classify_batch])
+        preds_classify = np.append(preds_classify, preds_classify_batch)
     # creating our df
     test_ids = [Path(fpath).stem for fpath in test_fpaths] # for the df
     if save_p:
@@ -104,7 +107,7 @@ def create_thresholded_classification_csv(pred_arr, test_ids, threshold=0.5):
     return sub_df
 
 def run_classification_prediction(x_test, classification_model, batch_size=32, tta=True,
-                                  n_tta_iter_per_image=4, preprocess_fn=None, **kwargs):
+                                  n_tta_iter_per_image=4, preprocess_fn=None):
     """
     Handles raw classification model prediction. Supports TTA and ensembling.
     Args:
@@ -116,8 +119,8 @@ def run_classification_prediction(x_test, classification_model, batch_size=32, t
         tta (boolean): whether or not to apply test-time augmentation.
         n_tta_iter_per_image (int): number of iterations of test-time augmentation with
             `data_aug.data_augmentation`. Defaults to 4.
-        preprocess_fn (function): function to preprocess the test arrays with. Specify the other arguments
-            with **kwargs. However, it must have the argument for x_test and the argument,`model_name`.
+        preprocess_fn (function): function to preprocess the test arrays with. It should only have
+            one argument, the input array. To use more arguments, use functools.partial.
     Returns:
         preds_classify (np.ndarray): shape (n, 1); assumes prediction channel is 1.
     """
@@ -128,13 +131,13 @@ def run_classification_prediction(x_test, classification_model, batch_size=32, t
         if isinstance(classification_model, (list, tuple)):
             # stacking across the batch_size dimension
             preds_classify = [TTA_Classification_All(model_, x_test, n_iter=n_tta_iter_per_image,
-                              batch_size=batch_size, preprocess_fn=None, **kwargs)
+                              batch_size=batch_size, preprocess_fn=preprocess_fn)
                               for model_ in classification_model]
             # ensembling by averaging
             preds_classify = np.mean(np.stack(preds_classify), axis=0).flatten()
         else:
             preds_classify = TTA_Classification_All(classification_model, x_test, n_iter=n_tta_iter_per_image,
-                                                    batch_size=batch_size).flatten()
+                                                    batch_size=batch_size, preprocess_fn=preprocess_fn).flatten()
     else:
         # ensembling without TTA
         if isinstance(classification_model, (list, tuple)):
@@ -145,15 +148,15 @@ def run_classification_prediction(x_test, classification_model, batch_size=32, t
             preds_classify = classification_model.predict(x_test, batch_size=batch_size).flatten()
     return preds_classify
 
-def TTA_Classification_All(model, test_arrays, n_iter=4, batch_size=32, seed=88, preprocess_fn=None, **kwargs):
+def TTA_Classification_All(model, test_arrays, n_iter=4, batch_size=32, seed=88, preprocess_fn=None):
     """
     Thin wrapper around for TTA Classification for TTA on all images instead of per image.
     """
     print("TTA for classification...")
     return np.asarray([TTA_Classification(model, test_arr, n_iter=n_iter, batch_size=batch_size,
-                       seed=seed, preprocess_fn=preprocess_fn, **kwargs) for test_arr in tqdm(test_arrays)])
+                       seed=seed, preprocess_fn=preprocess_fn) for test_arr in tqdm(test_arrays)])
 
-def TTA_Classification(model, test_array, n_iter=4, batch_size=32, seed=88, preprocess_fn=None, **kwargs):
+def TTA_Classification(model, test_array, n_iter=4, batch_size=32, seed=88, preprocess_fn=None):
     """
     Test-time augmentation with array inversion, horizontal and vertical flipping,
     gaussian smoothing, random rotations, and random zooms.
@@ -163,8 +166,8 @@ def TTA_Classification(model, test_array, n_iter=4, batch_size=32, seed=88, prep
         test_array (np.ndarray): shape of (H, W, C)
         batch_size: the batch size for prediction
         seed (int): desired seed for the data aug
-        preprocess_fn (function): function to preprocess the test arrays with. Specify the other arguments
-            with **kwargs. However, it must have the argument for x_test and the argument,`model_name`.
+        preprocess_fn (function): function to preprocess the test arrays with. It should only have
+            one argument, the input array. To use more arguments, use functools.partial.
     Returns:
         preds_test (np.ndarray): predicted probability for test_array (n_classes,)
     """
@@ -189,9 +192,8 @@ def TTA_Classification(model, test_array, n_iter=4, batch_size=32, seed=88, prep
         hflipped_img_array[each_iter+1] = data_augmentation(hflipped_img)[0] # no mask
     tmp_array = np.vstack((original_img_array, inverted_img_array, hflipped_img_array))
     # preprocessing
-    # default just converts the input from int -> flaot vvvvvvvvvvvvvvvvvvv
-    preprocess_fn = partial(preprocess_input, model_name=model_name) if preprocess_fn is None else preprocess_fn
-    tmp_array = preprocess_fn(tmp_array, model_name, **kwargs)
+    if preprocess_fn is not None:
+        tmp_array = preprocess_fn(tmp_array)
 
     n_classes = int(model.get_output_at(-1).get_shape()[1])
     prediction = np.mean(model.predict(tmp_array), axis=0)
