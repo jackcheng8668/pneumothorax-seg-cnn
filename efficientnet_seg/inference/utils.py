6fd2e7b7410ef9c1307a44aa7124bac0f6892139
jchen42703 <jchen42703@gmail.com>
2019-08-27 11:16:51 -0400

    Created post-processing functions, post_process_single and post_process_all
    Meant to replace the post-processing in the segmentation inference pipelines

--- efficientnet_seg/inference/utils.py
+++ efficientnet_seg/inference/utils.py
@@ -2,6 +2,50 @@ from PIL import Image
 import cv2
 import numpy as np
 
+def post_process_all(probability_masks, threshold=0.5, min_size=3500):
+    """
+    Thresholds, and zeroes out small ROIs. Applies to a stack of images instead of per image.
+    Args:
+        probability_mask (np.ndarray): squeezed output probability mask from a CNN,
+            with shape (n_images, h, w)
+        threshold (float): threshold value
+        min_size (int): minimum number of pixels for an ROI to be not zeroed out.
+    Returns:
+        predictions (np.ndarray): final thresholded array with small ROIs zeroed out, with
+            shape (n_images, 1024, 1024)
+    """
+    post_processed = [post_process_single(mask, threshold=threshold, min_size=min_size)
+                      for mask in probability_masks]
+    return np.stack(post_processed)
+
+def post_process_single(probability_mask, threshold=0.5, min_size=3500):
+    """
+    Thresholds, and zeroes out small ROIs (using the connected components algorithm)
+    Edited version of `post_process` in https://www.kaggle.com/rishabhiitbhu/unet-with-resnet34-encoder-pytorch?scriptVersionId=19433645.
+    Args:
+        probability_mask (np.ndarray): squeezed output probability mask from a CNN,
+            with shape (h, w)
+        threshold (float): threshold value
+        min_size (int): minimum number of pixels for an ROI to be not zeroed out.
+    Returns:
+        predictions (np.ndarray): final thresholded and transposed mask with small ROIs zeroed out, with
+            shape (1024, 1024). Max=255 and dtype=np.uint8.
+    """
+    h_w = (preds_seg.shape[0], preds_seg.shape[1])
+    if h_w != (1024, 1024):
+        probability_mask = cv2.resize(pred, (1024, 1024), interpolation=cv2.INTER_LINEAR)
+    # thresholding to 0s and 1s
+    mask = cv2.threshold(probability_mask, threshold, 1, cv2.THRESH_BINARY)[1]
+    num_component, component = cv2.connectedComponents(mask.astype(np.uint8))
+    predictions = np.zeros((1024, 1024), np.float32)
+    # iterating through each component and keeping it if it's > min_size
+    for c in range(1, num_component):
+        p = (component == c)
+        if p.sum() > min_size:
+            predictions[p] = 1
+    return (predictions.T*255).astype(np.uint8)
+
+
 def load_input(fpath, img_size=256, channels=3):
     """
     Loads and resizes a .png file.
